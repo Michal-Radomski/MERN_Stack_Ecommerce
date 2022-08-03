eaedbb7506471d2de668f7d7999ab46319db92f4
Michal Radomski <michalra@op.pl>
2022-08-03 22:26:20 +0200

    Handling errors was added

--- backend/server.ts
+++ backend/server.ts
@@ -4,6 +4,13 @@ const dotenv = require("dotenv");
 const app = require("./app");
 const connectDataBase = require("./config/database");
 
+// Handle Uncaught exceptions
+process.on("uncaughtException", (err) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting Down the Server Due to Uncaught Exception");
+  process.exit(1);
+});
+
 // Setting Up Config File
 dotenv.config({path: "./config/config.env"});
 
@@ -12,8 +19,17 @@ const port = (process.env.PORT || 5000) as number;
 // Connect to the DB
 connectDataBase();
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is listening at http://localhost:${port} in ${process.env.NODE_ENV} mode`);
   // For testing only
   console.log("Current Time:", new Date().toLocaleTimeString());
+});
+
+// Handle Unhandled Promise Rejections
+process.on("unhandledRejection", (err: Error) => {
+  console.log(`ERROR: ${err.stack}`);
+  console.log("Shutting Down the Server Due to Unhandled Promise Rejection");
+  server.close(() => {
+    process.exit(1);
+  });
 });
