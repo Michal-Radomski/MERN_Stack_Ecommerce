773ce05aed468ae6f0e8e2e5ca16a43b215ce882
Michal Radomski <michalra@op.pl>
2022-08-03 22:46:36 +0200

    Mongoose errors done

--- backend/middlewares/errors.ts
+++ backend/middlewares/errors.ts
@@ -4,7 +4,14 @@ import {Request, Response, NextFunction} from "express";
 const ErrorHandler = require("../utils/errorHandler");
 
 module.exports = (
-  err: {statusCode: number; message: string; stack: string},
+  err: {
+    errors(errors: string[]): string[];
+    name: string;
+    path: string;
+    statusCode: number;
+    message: string;
+    stack: string;
+  },
   _req: Request,
   res: Response,
   _next: NextFunction
@@ -23,6 +30,19 @@ module.exports = (
   if (process.env.NODE_ENV === "PRODUCTION") {
     let error = {...err};
     err.message = error.message;
+
+    // Wrong Mongoose Object ID Error
+    if (err.name === "CastError") {
+      const message = `Resource not found. Invalid: ${err.path}`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling Mongoose Validation Error
+    if (err.name === "ValidationError") {
+      const message = Object.values(err.errors).map((value) => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(error.statusCode).json({
       success: false,
       message: error.message || "Internal Sever Error",
