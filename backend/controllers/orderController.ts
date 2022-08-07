068603611231f9807ff2707083c435f171427f91
Michal Radomski <michalra@op.pl>
2022-08-07 19:11:46 +0200

    Get single order and logged in user orders done

--- backend/controllers/orderController.ts
+++ backend/controllers/orderController.ts
@@ -34,3 +34,29 @@ exports.newOrder = catchAsyncErrors(async (req: CustomRequest, res: Response) =>
     order: order,
   });
 });
+
+// Get single order   =>   /api/v1/order/:id
+exports.getSingleOrder = catchAsyncErrors(async (req: Request, res: Response, next: NextFunction) => {
+  const order = await Order.findById(req.params.id).populate("user", "name email");
+  // console.log({order});
+
+  if (!order) {
+    return next(new ErrorHandler("No Order found with this ID", 404));
+  }
+
+  res.status(200).json({
+    success: true,
+    order: order,
+  });
+});
+
+// Get logged in user orders   =>   /api/v1/orders/me
+exports.myOrders = catchAsyncErrors(async (req: CustomRequest, res: Response) => {
+  const orders = await Order.find({user: req.user.id});
+  // console.log({orders});
+
+  res.status(200).json({
+    success: true,
+    orders,
+  });
+});
