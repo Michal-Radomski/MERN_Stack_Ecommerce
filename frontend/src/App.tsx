61b1377b708fc971fa294ea488dea6ce6b3e9346
Michal Radomski <michalra@op.pl>
2022-08-09 22:21:21 +0200

    Header, Footer done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
 import React from "react";
+
+import Header from "./components/layouts/Header";
 import "./App.scss";
+import Footer from "./components/layouts/Footer";
 
 function App() {
-  return <React.Fragment>App</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Header />
+      <Footer />
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/layouts/Footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <React.Fragment>
+      <footer className="py-1">
+        <p className="text-center mt-1">Shopping Cart - {new Date().getFullYear()}, All Rights Reserved</p>
+      </footer>
+    </React.Fragment>
+  );
+};
+
+export default Footer;
