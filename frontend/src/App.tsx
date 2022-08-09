7d32db5f944ebefa32ce4466859a3c8d820ee1e7
Michal Radomski <michalra@op.pl>
2022-08-09 22:38:55 +0200

    Home component done

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import React from "react";
 
-import Header from "./components/layouts/Header";
 import "./App.scss";
+import Header from "./components/layouts/Header";
 import Footer from "./components/layouts/Footer";
+import Home from "./components/Home";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Home />
       <Footer />
     </React.Fragment>
   );
