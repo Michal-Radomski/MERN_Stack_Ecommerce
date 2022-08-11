873330f80cce7fef2487a794443c284b9580eefd
Michal Radomski <michalra@op.pl>
2022-08-11 14:23:56 +0200

    Register user - reducer + action done

--- frontend/src/redux/actions/userActions.ts
+++ frontend/src/redux/actions/userActions.ts
@@ -1,6 +1,14 @@
 import axios from "axios";
 
-import {CLEAR_ERRORS, LOGIN_FAIL, LOGIN_REQUEST, LOGIN_SUCCESS} from "../constants/userConstants";
+import {
+  CLEAR_ERRORS,
+  LOGIN_FAIL,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
+} from "../constants/userConstants";
 
 // Login
 export const login = (email: string, password: string) => async (dispatch: Dispatch) => {
@@ -32,4 +40,29 @@ export const clearErrors = () => async (dispatch: Dispatch) => {
   dispatch({
     type: CLEAR_ERRORS,
   });
+};
+
+// Register user
+export const register = (userData: Object) => async (dispatch: Dispatch) => {
+  try {
+    dispatch({type: REGISTER_USER_REQUEST});
+
+    const config = {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    };
+
+    const {data} = await axios.post("/api/v1/register", userData, config);
+
+    dispatch({
+      type: REGISTER_USER_SUCCESS,
+      payload: data.user,
+    });
+  } catch (error) {
+    dispatch({
+      type: REGISTER_USER_FAIL,
+      payload: (error as CustomError).response.data.message,
+    });
+  }
 };
