46a42203d3d979eb3a2445b4d018ed7573e2b92d
Michal Radomski <michalra@op.pl>
2022-08-14 14:46:43 +0200

    Delete user -backend  done

--- backend/controllers/authController.ts
+++ backend/controllers/authController.ts
@@ -293,7 +293,9 @@ exports.deleteUser = catchAsyncErrors(async (req: Request, res: Response, next: 
     return next(new ErrorHandler(`User does not found with id: ${req.params.id}`));
   }
 
-  //TODO: Remove avatar
+  // Remove avatar from cloudinary
+  const image_id = user.avatar.public_id;
+  await cloudinary.v2.uploader.destroy(image_id);
 
   await user.remove();
 
